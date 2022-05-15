052700c517d79dd0fc4bd4d9863655aed0d4025a
Ray <rayrlam@yahoo.com.hk>
2022-05-15 15:01:58 +0100

    update modeles

--- app/Models/Rule.php
+++ app/Models/Rule.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Rule extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'item_id',
+        'method',
+        'qtyorid',
+        'sprice',
+        'eprice',
+        'is_consistent',
+        'expirydate'
+    ];
 }
