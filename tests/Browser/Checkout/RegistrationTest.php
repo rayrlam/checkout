a0e2acee961c1d60d8f47d61cc9db18b5224887d
Ray Lam <raylamedeas@gmail.com>
2024-09-04 18:55:39 +0100

    Add 2 Test for Registration Page

--- tests/Browser/Checkout/RegistrationTest.php
+++ tests/Browser/Checkout/RegistrationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Browser\Checkout;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Sleep;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
@@ -11,7 +12,7 @@ class RegistrationTest extends DuskTestCase
     /**
      * A Dusk test Registration
      */
-    public function testRegistration(): void
+    public function test_register_has_name_email_password_confirm_password(): void
     {
         $this->browse(function (Browser $browser) {
             $browser
@@ -19,7 +20,26 @@ class RegistrationTest extends DuskTestCase
                 ->assertSee('Name')
                 ->assertSee('Email')
                 ->assertSee('Password')
-                ->assertSee('Confirm Password');           
+                ->assertSee('Confirm Password');      
+        });
+    }
+
+     /**
+     * A Dusk test Registration
+     */
+    public function test_password_at_least_8_characters(): void
+    {
+        $this->browse(function (Browser $browser) {
+               
+            $browser->type('name','Peter');
+            $browser->type('email','peter@example.com');
+            // Only input 4 characters for password 
+            $browser->type('password', '1234');
+            $browser->type('password_confirmation', '1234');
+            $browser->press('REGISTER');
+
+            // AssesrtSee - The password must be at least 8 characters.
+            $browser->assertSee('8 characters');
         });
     }
 }
