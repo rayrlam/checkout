14c83374b5a5afffc6f69d8d547e976d59e816ab
Ray Lam <raylamedeas@gmail.com>
2024-09-08 13:25:55 +0100

    Change the name RegistrationTest to CheckoutTest & add a login test

--- tests/Browser/Checkout/CheckoutTest.php
+++ tests/Browser/Checkout/CheckoutTest.php
@@ -2,15 +2,18 @@
 
 namespace Tests\Browser\Checkout;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Sleep;
 use Laravel\Dusk\Browser;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 use Tests\DuskTestCase;
 
-class RegistrationTest extends DuskTestCase
+class CheckoutTest extends DuskTestCase
 {
     /**
-     * A Dusk test Registration
+     * A Dusk test Registration has name, email, password & confirm passowrd 
      */
     public function test_register_has_name_email_password_confirm_password(): void
     {
@@ -24,8 +27,8 @@ class RegistrationTest extends DuskTestCase
         });
     }
 
-     /**
-     * A Dusk test Registration
+    /**
+     * A Dusk test Registration password at least 8 characters
      */
     public function test_password_at_least_8_characters(): void
     {
@@ -44,6 +47,33 @@ class RegistrationTest extends DuskTestCase
                 ->press('REGISTER')
                 // AssesrtSee - The password must be at least 8 characters.
                 ->assertSee('8 characters');
+        });
+    }
+
+     /**
+     * A Dusk test Registration user can login and see dashboard
+     */
+    public function test_user_can_login_and_see_dashboard(): void
+    {
+        $name = fake()->name;
+        $email = fake()->email;
+        $password = substr(fake()->password, 0, 8); 
+
+        User::factory()->create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password), 
+        ]);
+ 
+        $this->browse(function (Browser $browser) use ($email, $password) {
+            $browser->visit('/login')
+                    ->type('email', $email)
+                    ->type('password', $password)
+                    ->press('button[type="submit"]')
+                    ->assertPathIs('/dashboard')
+                    ->assertSee('Dashboard')
+                    ->assertSee('Create Item')
+                    ->assertSee('Create Rule');
         });
     }
 }
