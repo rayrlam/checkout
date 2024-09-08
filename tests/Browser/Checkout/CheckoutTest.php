9b70a6e3b903c569a09dde6514293d0e90eb141d
Ray Lam <raylamedeas@gmail.com>
2024-09-08 13:38:47 +0100

    Add tests for special price rules

--- tests/Browser/Checkout/CheckoutTest.php
+++ tests/Browser/Checkout/CheckoutTest.php
@@ -50,7 +50,7 @@ class CheckoutTest extends DuskTestCase
         });
     }
 
-     /**
+    /**
      * A Dusk test Registration user can login and see dashboard
      */
     public function test_user_can_login_and_see_dashboard(): void
@@ -74,6 +74,72 @@ class CheckoutTest extends DuskTestCase
                     ->assertSee('Dashboard')
                     ->assertSee('Create Item')
                     ->assertSee('Create Rule');
+        });
+    }
+
+    /**
+     * A Dusk test Registration 'A' x 3, get total 130
+     */
+    public function test_3a_checkout_get_130(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/tasks/checkout/calculator')
+                    ->type('items[1]', 3)
+                    ->press('button[type="submit"]')
+                    ->assertSee('130');
+        });
+    }
+
+    /**
+     * A Dusk test Registration 'B' x 2, get total 45
+     */
+    public function test_2a_checkout_get_45(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/tasks/checkout/calculator')
+                    ->type('items[2]', 2)
+                    ->press('button[type="submit"]')
+                    ->assertSee('45');
+        });
+    }
+
+    /**
+     * A Dusk test Registration 'C' x 2, get total 38
+     */
+    public function test_2c_checkout_get_38(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/tasks/checkout/calculator')
+                    ->type('items[3]', 2)
+                    ->press('button[type="submit"]')
+                    ->assertSee('38');
+        });
+    }
+
+    /**
+     * A Dusk test Registration 'C' x 3, get total 50
+     */
+    public function test_3c_checkout_get_50(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/tasks/checkout/calculator')
+                    ->type('items[3]', 3)
+                    ->press('button[type="submit"]')
+                    ->assertSee('50');
+        });
+    }
+
+    /**
+     * A Dusk test Registration total get 5 if purchased with an ‘A’
+     */
+    public function test_d_checkout_with_3a_get_135(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/tasks/checkout/calculator')
+                    ->type('items[1]', 3)
+                    ->type('items[4]', 1)
+                    ->press('button[type="submit"]')
+                    ->assertSee('135');
         });
     }
 }
