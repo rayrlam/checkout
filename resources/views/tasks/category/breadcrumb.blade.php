7f5725b49379b0d9879db399c91adf0979d2b7b4
Ray Lam <raylamedeas@gmail.com>
2024-09-10 22:27:42 +0100

    Change breadcrumb category from input to select

--- resources/views/tasks/category/breadcrumb.blade.php
+++ resources/views/tasks/category/breadcrumb.blade.php
@@ -13,15 +13,18 @@
                 <div class="p-2 md:p-4">
                     <label for="id" class="block mb-2 text-xs font-medium text-gray-900 dark:text-white">Select Category</label>
                     <select id="id" name="id" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-
                         @foreach($list as $category)
                             <option value={{ $category->id }}>{{ $category->name }}</option>
                         @endforeach    
                     </select>
 
                     <div class="mt-3">
-                        <label for="separator" class="block mb-2 text-xs font-medium text-gray-900 dark:text-white">Small input</label>
-                        <input type="text" id="separator" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value=">">
+                        <label for="separator" class="block mb-2 text-xs font-medium text-gray-900 dark:text-white">Select Category</label>
+                        <select id="separator" name="sep" class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            @foreach(['>','/','|','\\','*','@'] as $value)
+                                <option value={{ $value }}>{{ $value }}</option>
+                            @endforeach    
+                        </select>
                     </div>
 
                     <p class="mt-3 block text-xs font-medium text-gray-900 dark:text-white">Breadcrumb</p>
@@ -32,7 +35,7 @@
                 </div>
 
                 <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
-                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1.5 text-center text-xs" type="submit">
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1.5 text-center">
                         Create
                     </button>
                 </div>
--- resources/views/tasks/category/categories.blade.php
+++ resources/views/tasks/category/categories.blade.php
@@ -1,44 +1,62 @@
 <x-home>
-    <div class="p-8 w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <div class="flex" style="justify-content:space-between; align-items:baseline">
-            <h2 class="font-bold lg:text-3xl text-2xl mt-4 mb-4">
-                {{ __('Categories') }}
-            </h2>
-        </div>  
-
-        <div class="">
-            <h4>List Data By Parent Id {{ $id }}</h4>    
-            <div class="mt-4">
-                <table id="items">
-                    <tr>
-                        <th>id</th>
-                        <th>parent_id</th>
-                        <th style="min-width:200px">name</th>
-                    </tr>
-                    @forelse($categories as $c)
-                    <tr>
-                        <td>
-                            {{ $c->id }}
-                        </td>
-                        <td>
-                            {{ $c->parent_id }}
-                        </td>
-                        <td>
-                            {{ $c->name }}
-                        </td>
-                    </tr>
-                    @empty
-                    <tr>
-                        <td colspan="3">Category Not Found</td>
-                    </tr>
-                    @endforelse    
+    <x-slot name="title">
+        {{ $title ?? null }}
+    </x-slot>
+    <div class="block">  
+       <h3 class="text-lg font-bold dark:text-white">
+            {{ __('Categories') }}
+        </h3>
+ 
+        <p class="text-xs">List Data By Parent Id: {{ $id }}</p>    
+        <div class="mt-4">
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
+                <table id="items" class="w-full text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-center">
+                                Id
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-center">
+                                Parent Id
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-center">
+                                Name
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($categories as $index => $c)
+                            <tr 
+                                class="
+                                    @if($index%2 ==0)
+                                        bg-white dark:bg-gray-900  border-b dark:border-gray-700
+                                    @else
+                                        bg-gray-50 dark:bg-gray-800 border-b dark:border-gray-700
+                                    @endif
+                                ">
+                                <th scope="row" class="text-center px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white text-xs">
+                                    {{ $c->id }}
+                                </td>
+                                <td class="text-center px-6 py-2 text-xs">
+                                    {{ $c->parent_id }}
+                                </td>
+                                <td class="text-center px-6 py-2 text-xs">
+                                    {{ $c->name }}
+                                </td>
+                            </tr>
+                        @empty
+                            <tr class="bg-white dark:bg-gray-900  border-b dark:border-gray-700">
+                                <td colspan="3">Category Not Found</td>
+                            </tr>
+                        @endforelse    
+                    </tbody>
                 </table>
             </div>
         </div>
 
         <form class="ml-4 mt-8" method="post" action="{{route('tasks.category.categories')}}">
             @csrf
-            <p class="mt-6 mb-2">
+            <p class="mt-6 mb-2 text-sm">
                 Category Parent Id
             </p>
             <select name="id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -46,8 +64,11 @@
                     <option value="{{ $p->parent_id }}">{{ $p->parent_id }}</option>
                 @endforeach
             </select>
+
             <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
-                <button type="submit" class="mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1.5 text-center">
+                    Submit
+                </button>
             </div>
         </form>
     </div>
