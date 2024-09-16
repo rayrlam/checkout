78c6788edcca98f643e4da49e6f6c4ee4def424a
Ray Lam <raylamedeas@gmail.com>
2024-09-16 21:48:46 +0100

    Update test for PHP 8.2

--- tests/Unit/PHP/PHP8_2_Test.php
+++ tests/Unit/PHP/PHP8_2_Test.php
@@ -15,4 +15,42 @@ class PHP8_2_Test extends TestCase
         $this->assertEquals(100, MyClass::MAX_VALUE);
         $this->assertIsInt(MyClass::MAX_VALUE);
     }
+
+    public function test_readonly_classes()
+    {
+        $user = new class('John', 30) {
+            public function __construct(
+                public readonly string $name,
+                public readonly int $age
+            ) {}
+        };
+
+        $this->assertEquals('John', $user->name);
+        $this->assertEquals(30, $user->age);
+
+        $this->expectException(\Error::class);
+        $user->name = 'Peter';
+    }
+
+    public function test_null_false_and_true_as_standalone_types()
+    {
+        $testNull = function(null $value): string {
+            return "It's null!";
+        };
+
+        $testFalse = function(false $value): string {
+            return "It's false!";
+        };
+
+        $testTrue = function(true $value): string {
+            return "It's true!";
+        };
+
+        $this->assertEquals("It's null!", $testNull(null));
+        $this->assertEquals("It's false!", $testFalse(false));
+        $this->assertEquals("It's true!", $testTrue(true));
+
+        $this->expectException(\TypeError::class);
+        $testNull('not null');
+    }
 }
