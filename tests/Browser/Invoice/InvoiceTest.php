712cb0d0dcef503e15869e8cb0343270ee73041c
Ray Lam <raylamedeas@gmail.com>
2024-10-10 18:36:16 +0100

    Add some dusk tests for invoice pages

---
+++ tests/Browser/Invoice/InvoiceTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Browser\Checkout;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Sleep;
+use Laravel\Dusk\Browser;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use Tests\DuskTestCase;
+
+class InvoiceTest extends DuskTestCase
+{
+    public function test_can_see_invoice_index(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit('/tasks/invoice/index')
+                ->assertSee('Invoice')
+                ->assertSee('Info')
+                ->assertSee('Setup');  
+        });
+    }
+
+    public function test_can_see_invoice_headers(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit('/tasks/invoice/headers')
+                ->assertSee('Headers')
+                ->assertSee('Range Date')
+                ->assertSee('Location')  
+                ->assertSee('Submit');  
+        });
+    }
+
+    public function test_can_see_invoice_location(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit('/tasks/invoice/location')
+                ->assertSee('Status')
+                ->assertSee('Total')
+                ->assertSee('Submit');  
+        });
+    }
+
+    public function test_can_see_invoice_total(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit('/tasks/invoice/total')
+                ->assertSee('Total')
+                ->assertSee('Quantity');
+        });
+    }
+}
